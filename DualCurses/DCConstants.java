e216d72cd2fb76c1ec9cbf4f1e12ce260380cc58
Michael <MichaelWidler@yahoo.com>
2024-06-10 06:53:39 -0700

    Add constants file.

---
+++ DualCurses/DCConstants.java
@@ -0,0 +1,10 @@
+package DualCurses;
+
+import java.awt.*;
+
+
+public interface DCConstants
+{
+   public static final int DEFAULT_FG_COLOR = Color.WHITE.getRGB();
+   public static final int DEFAULT_BG_COLOR = Color.BLACK.getRGB();
+}
